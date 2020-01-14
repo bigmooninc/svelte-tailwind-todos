1b6a32d4b4420ab8155723e35a0c96b6d25dd1ae
bigmooninc <aaron@bigmooninc.com>
2020-01-14 17:32:58 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+</script>
+
+<main class="p-10">
+	<h1 class="text-center uppercase">Hello {name}!</h1>
+	<p class="text-center">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		max-width: 240px;
+		@apply mx-auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
